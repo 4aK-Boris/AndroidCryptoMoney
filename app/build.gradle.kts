d10f54dd85f63741d4c5ea8af851b9af2b70facf
Дмитрий <dmitriylosevxxx@gmail.com>
2022-11-25 18:03:47 +0300

    Создание проекта

---
+++ app/build.gradle.kts
@@ -0,0 +1,76 @@
+plugins {
+    id(Dependencies.Plugins.application)
+    id(Dependencies.Plugins.kotlin)
+    kotlin(Dependencies.Plugins.serialization) version Dependencies.Kotlin.version
+}
+
+android {
+    namespace = Dependencies.Application.namespace
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = Dependencies.Application.namespace
+        minSdk = 28
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Dependencies.Compose.kotlinCompilerExtensionVersion
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(Dependencies.Android.core)
+    implementation(Dependencies.Android.lifecycle)
+    implementation(Dependencies.Android.activity)
+
+    implementation(Dependencies.Compose.ui)
+    implementation(Dependencies.Compose.preview)
+    implementation(Dependencies.Compose.material)
+
+    implementation(Dependencies.Ktor.client)
+
+    implementation(Dependencies.Kotlin.serialization)
+
+    implementation(Dependencies.Koin.koin)
+
+    implementation(Dependencies.Kotlin.coroutines)
+
+    testImplementation(Dependencies.JUnit.jUnit)
+
+    androidTestImplementation(Dependencies.JUnit.extJUnit)
+
+    androidTestImplementation(Dependencies.Compose.jUnit)
+
+    debugImplementation(Dependencies.Compose.tooling)
+    debugImplementation(Dependencies.Compose.manifest)
+}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,65 @@
+object Dependencies {
+
+    object Application {
+        const val namespace = "aleksandr.fedotkin"
+    }
+
+    object Android {
+        const val core = "androidx.core:core-ktx:1.9.0"
+        const val lifecycle = "androidx.lifecycle:lifecycle-runtime-ktx:2.5.1"
+        const val activity = "androidx.activity:activity-compose:1.6.1"
+    }
+
+    object Compose {
+
+        private const val version = "1.3.1"
+        const val kotlinCompilerExtensionVersion = "1.3.2"
+
+        const val ui = "androidx.compose.ui:ui:$version"
+        const val preview = "androidx.compose.ui:ui-tooling-preview:$version"
+        const val material = "androidx.compose.material:material:$version"
+
+        const val jUnit = "androidx.compose.ui:ui-test-junit4:$version"
+
+        const val tooling = "androidx.compose.ui:ui-tooling:$version"
+        const val manifest = "androidx.compose.ui:ui-test-manifest:$version"
+    }
+
+    object JUnit {
+        const val jUnit = "junit:junit:4.13.2"
+        const val extJUnit = "androidx.test.ext:junit:1.1.4"
+    }
+
+    object Ktor {
+
+        private const val version = "2.1.3"
+
+        const val client = "io.ktor:ktor-client-android:$version"
+    }
+
+    object Kotlin {
+
+        const val version = "1.7.10"
+
+        const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4"
+        const val serialization = "org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1"
+    }
+
+    object Koin {
+
+        private const val version = "3.3.0"
+
+        const val koin = "io.insert-koin:koin-android:$version"
+    }
+
+    object Plugins {
+
+        const val gradleVersion = "7.3.1"
+
+        const val application = "com.android.application"
+        const val library = "com.android.library"
+        const val kotlin = "org.jetbrains.kotlin.android"
+        const val serialization = "plugin.serialization"
+    }
+
+}
